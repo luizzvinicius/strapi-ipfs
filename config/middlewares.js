39001c9e0f23fe4527e8f489f934cc95a4437bf8
Luiz Vinícius <viniciusadr244@gmail.com>
2023-12-19 11:50:22 -0300

    add .env

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,6 +1,35 @@
 module.exports = [
   'strapi::errors',
-  'strapi::security',
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "http:"],
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "dl.airtable.com",
+            "*.ipfs.dweb.link", // ipfs.tech
+            "*.ipfs.cf-ipfs.com", // cloudflare.com
+            "*.ipfs.w3s.link", // web3.storage
+          ],
+          "media-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "dl.airtable.com",
+            "*.ipfs.dweb.link", // ipfs.tech
+            "*.ipfs.cf-ipfs.com", // cloudflare.com
+            "*.ipfs.w3s.link", // web3.storage
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::logger',
