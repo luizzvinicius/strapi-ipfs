e9a6c6a2d347369d172d90e63ed1f664b67f00c0
Luiz Vinícius <viniciusadr244@gmail.com>
2023-12-20 11:07:41 -0300

    feat: upload & delete ok

--- providers/strapi-provider-upload-ipfs-alex/index.js
+++ providers/strapi-provider-upload-ipfs-alex/index.js
@@ -1,44 +1,49 @@
 let client;
 module.exports = {
-  init(config) {    
+  init(config) {
     import('kubo-rpc-client').then(({ create }) => {
-      client = create(`http://${config.grpc}`);
+      client = create({
+        host: "127.0.0.1",
+        port: 5001,
+      });
     });
     return {
       async uploadStream(file) {
         try {
-          const ipfs_status = await client.files.write(
+          const name = file.name
+          const data = file.stream
+          await client.files.write(
             `/${file.name}`,
-            file.stream,
+            data,
             { create: true }
           );
-          console.log("IPFS Status", ipfs_status);
-        } catch (e) {
-          console.log(e);
-        }
-      },
-      async upload(file) {
-        try {
-          const ipfs_status = await client.files.write(
-            `/${file.name}`,
-            file.stream,
-            { create: true }
-          );
-          console.log("IPFS Status", ipfs_status);
-        } catch (e) {
-          console.log(e);
-        }
-      },
-      delete(file) {
-        console.log("Deleting", file);
-        return new Promise(async (resolve, reject) => {
-          try {
-            await client.pin.rm(file.path);
-            resolve("teste");
-          } catch (e) {
-            reject(e);
+          let info = []
+          for await (const chunck of client.files.ls(`/${name}`)) {
+            info.push(chunck)
           }
-        })
+          await client.pin.add(info[0].cid)
+        } catch (e) {
+          console.log(e);
+        }
+      },
+      async upload(file) { // Não usado pelo strapi
+        try {
+          await client.files.write(
+            `/${file.name}`,
+            file.stream,
+            { create: true }
+          );
+        } catch (e) {
+          console.log(e);
+        }
+      },
+      async delete(file) {
+        console.log("Deleting", file);
+        try {
+          await client.files.rm(`/${file.name}`)
+        } catch (e) {
+          console.log(e)
+        }
       },
     };
   },
